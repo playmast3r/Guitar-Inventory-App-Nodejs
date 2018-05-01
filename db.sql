32f1934de3e8695e84ca1897fdc739f2f4f77369
Siddharth Mahajan <playmast3r@users.noreply.github.com>
2018-05-01 09:17:26 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,3 +1,11 @@
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ProductList`
+--
+
+
+
 CREATE TABLE `ProductList` (
   `pId` INT NOT NULL AUTO_INCREMENT,
   `brand` VARCHAR(150) NOT NULL,
@@ -6,3 +14,27 @@ CREATE TABLE `ProductList` (
   `numOfString` MEDIUMINT(11) NULL,
   `price` FLOAT NOT NULL,
   PRIMARY KEY (`pId`));
+  
+ 
+ -- --------------------------------------------------------
+
+--
+-- Table structure for table `Inventory`
+--
+
+
+  CREATE TABLE `Inventory` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `pId` INT(11) NULL,
+  `uniqueSerialNumber` VARCHAR(100) NOT NULL,
+  `buyDate` DATE NOT NULL,
+  `sellDate` DATE NULL,
+  `accessoriesSold` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_Inventory_1_idx` (`pId` ASC),
+  CONSTRAINT `fk_Inventory_1`
+    FOREIGN KEY (`pId`)
+    REFERENCES `ProductList` (`pId`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
